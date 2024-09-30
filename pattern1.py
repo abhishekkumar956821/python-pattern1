2c74b12ba48a60d8bdba1749f6a0bc6669518372
Abhishek kumar <116196351+abhishekkumar956821@users.noreply.github.com>
2024-09-30 11:01:03 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,23 @@
+n= 7
+
+for k in range(n, 0, -1):
+
+    digit = k
+
+    for m in range(0, k):
+
+        print(digit, end=' ')
+
+    print("\r")
+
+
+
+
+# output
+# 7 7 7 7 7 7 7 
+# 6 6 6 6 6 6
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
